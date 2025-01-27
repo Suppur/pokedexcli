eb0d5be9a022ad1b6a60b0c68689dca5fe539e69
Suppur <zi213@hotmail.com>
2025-01-27 00:03:31 -0500

    added cache

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,67 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entries  map[string]cacheEntry
+	mutex    sync.RWMutex
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := &Cache{
+		entries:  make(map[string]cacheEntry),
+		interval: interval,
+	}
+
+	go cache.reapLoop()
+
+	return cache
+}
+
+func (c *Cache) add(key string, val []byte) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) get(key string) ([]byte, bool) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+
+	entry, ok := c.entries[key]
+	if !ok {
+		return nil, false
+	}
+
+	return entry.val, true
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+
+	for {
+		<-ticker.C
+
+		c.mutex.Lock()
+		for key, entry := range c.entries {
+			if time.Since(entry.createdAt) > c.interval {
+				delete(c.entries, key)
+			}
+		}
+		c.mutex.Unlock()
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"time"
+
+	"github.com/Suppur/pokedexcli/internal/pokecache"
+)
+
+func cacheInit() *pokecache.Cache {
+	cache := pokecache.NewCache(5 * time.Second)
+	return cache
+}
+
 func main() {
+	cacheInit()
 	replInit()
+
 }
